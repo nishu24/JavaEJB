fd5b7b1294c43ed1da2c058925192e0f81d7ce52
Nisarg <Nisarg@Nishu.gateway.2wire.net>
2015-03-28 16:20:36 -0400

    session bean initialized

--- JavaEJB-ejb/src/java/com/javaProject/stateless/SessionEJBean.java
+++ JavaEJB-ejb/src/java/com/javaProject/stateless/SessionEJBean.java
@@ -18,4 +18,19 @@ public class SessionEJBean {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int subtract(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public int division(int number1, int number2) {
+        return number1 / number2;
+    }
 }
